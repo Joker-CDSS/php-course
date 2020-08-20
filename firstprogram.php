5cdf28bf0e3bbc83ebd0bd8584a53c0eed3975ff
Joker-CDSS <samuelinokon@gmail.com>
2020-08-20 20:02:25 +0100

    initial commit

---
+++ firstprogram.php
@@ -0,0 +1,18 @@
+<html>
+
+ <head>
+ <h1>First Program</h1>
+ </head>
+
+  <body>
+
+   <?php
+
+    echo "this is my first php code on XAMPP welcome Joker warmly";
+
+   
+   ?>
+
+  </body>
+
+</html>
